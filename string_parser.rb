130d176a5288f7002d78d9a49592e79b0a2edff2
Debashis Giri <giri.debu@gmail.com>
2025-02-28 11:39:05 +0530

    Initial files

---
+++ string_parser.rb
@@ -0,0 +1,9 @@
+class StringParser
+  def self.add(num_str)
+    if num_str.empty?
+      return 0
+    else
+      # Wip
+    end
+  end
+end
---
+++ string_parser_test.rb
@@ -0,0 +1,16 @@
+require 'minitest/autorun'
+require_relative 'string_parser'
+
+class StringParserTest < Minitest::Test
+  def test_empty_string
+    assert_equal 0, StringParser.add('')
+  end
+
+  def test_with_two_numbers
+    assert_equal 3, StringParser.add('1,2')
+  end
+
+  def test_multiple_numbers
+    assert_equal 15, StringParser.add('1,2,3,4,5')
+  end
+end
